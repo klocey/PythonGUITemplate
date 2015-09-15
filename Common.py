8fbac2c4ccc80445ee9099b8aaabd6a46b5f76cb
klocey <ken@weecology.org>
2015-09-15 16:27:09 -0400

    added directory and files for a Hydrobide GUI (not complete) -KL

--- Common.py
+++ Common.py
@@ -6,10 +6,12 @@ from enthought.traits.api import *
 from enthought.traits.ui.api import View,UItem, Item, Group, Heading, Label, \
         HSplit, Handler, CheckListEditor, EnumEditor, TableEditor, \
         ListEditor, Tabbed, VGroup, HGroup, RangeEditor, Spring, spring
+
 from enthought.traits.ui.menu import NoButtons
 #from enthought.traits.api import Any, Instance
 from enthought.traits.ui.wx.editor import Editor
 from enthought.traits.ui.wx.basic_editor_factory import BasicEditorFactory
+
 import numpy as np
 from enthought.enable.api import ColorTrait
 
@@ -35,18 +37,18 @@ class _MPLFigureEditor(Editor):
     def _create_canvas(self, parent):
         """ Create the MPL canvas. """
         # The panel lets us add additional controls.
-        
+
         panel = wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
         sizer = wx.BoxSizer(wx.VERTICAL)
         panel.SetSizer(sizer)
-        
+
         # matplotlib commands to create a canvas
         mpl_control = FigureCanvas(panel, -1, self.value)
         sizer.Add(mpl_control, 1, wx.LEFT | wx.TOP | wx.GROW)
         toolbar = NavigationToolbar2Wx(mpl_control)
         sizer.Add(toolbar, 0, wx.EXPAND)
         self.value.canvas.SetMinSize((10,10))
-        
+
         return panel
 
 class MPLFigureEditor(BasicEditorFactory):
--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from Common import *
 class ApplicationMain(HasTraits):
 
     scene = Instance(MlabSceneModel, ())
-    
+
     firstcalc = Instance(FirstCalc)
     secondcalc = Instance(SecondCalc)
     display = Instance(Figure)
     markercolor = ColorTrait
     markerstyle = Enum(['+',',','*','s','p','d','o'])
     markersize = Range(0,10,2)
- 
+
     left_panel  = Tabbed(Group(VGroup(Item('display', editor=MPLFigureEditor(),show_label=False, resizable=True)),
                                HGroup(Item(name='markercolor', label="Color", style="custom",springy=True),
                                       Item(name='markerstyle', label="Marker",springy=True),
@@ -27,7 +27,7 @@ class ApplicationMain(HasTraits):
 
     right_panel = Tabbed(Item('firstcalc', style='custom', label='First Tab',show_label=False),
                          Item('secondcalc', style='custom', label='Second Tab',show_label=False))
-    
+
     view = View(HSplit(left_panel,
                  right_panel),
                 width = 1280,
@@ -45,7 +45,7 @@ class ApplicationMain(HasTraits):
         ax.set_ylabel('Y')
         ax.set_xlim(0,1)
         ax.set_ylim(0,1)
- 
+
         # Set matplotlib canvas colour to be white
         rect = figure.patch
         rect.set_facecolor('w')
@@ -63,20 +63,20 @@ class ApplicationMain(HasTraits):
 
     def _markercolor_changed(self):
         ax = self.display.axes[0]
-        if hasattr(self, 'display_points'): 
+        if hasattr(self, 'display_points'):
             self.display_points.set_color(self.markercolor)
             self.display_points.set_markeredgecolor(self.markercolor)
             wx.CallAfter(self.display.canvas.draw)
 
     def _markerstyle_changed(self):
         ax = self.display.axes[0]
-        if hasattr(self, 'display_points'): 
+        if hasattr(self, 'display_points'):
             self.display_points.set_marker(self.markerstyle)
             wx.CallAfter(self.display.canvas.draw)
 
     def _markersize_changed(self):
         ax = self.display.axes[0]
-        if hasattr(self, 'display_points'): 
+        if hasattr(self, 'display_points'):
             self.display_points.set_markersize(self.markersize)
             wx.CallAfter(self.display.canvas.draw)
 
